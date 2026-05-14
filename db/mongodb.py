2e851d7e59efdd18ca6a04c71543b56df0171ba2
hodoon <ehgns79513@gmail.com>
2026-05-14 09:05:34 +0900

    feat: generate_workflow_logs 컬렉션 신규 추가

--- db/mongodb.py
+++ db/mongodb.py
@@ -5,3 +5,4 @@ client = AsyncIOMotorClient(settings.MONGODB_URL)
 db = client[settings.MONGODB_DB_NAME]
 
 execution_logs = db["execution_logs"]
+generate_workflow_logs = db["generate_workflow_logs"]
