f90ac830f2ec85ae8483c8ef9b6388ac52d4a2f7
hodoon <ehgns79513@gmial.com>
2026-05-14 14:05:01 +0900

    refactor: AgentException에 status_code 속성 추가

--- common/exception.py
+++ common/exception.py
@@ -4,5 +4,6 @@ from common.error_code import ErrorCode
 class AgentException(Exception):
     def __init__(self, error_code: ErrorCode):
         self.error_code = error_code
+        self.status_code = error_code.status_code
         self.message = error_code.message
         super().__init__(self.message)
